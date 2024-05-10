823ce77d6e363df8a18885be4e2fd61baf221f09
divyanshuraj751 <146466712+divyanshuraj751@users.noreply.github.com>
2024-05-10 22:35:26 +0530

    Update spiral.cpp

--- spiral.cpp
+++ spiral.cpp
@@ -16,28 +16,24 @@ vector<int> spiralOrder(vector<vector<int>>& matrix) {
     int endingCol = col-1;
 
     while(count<total) {
-        //print starting row
         for(int index = startingCol;count<total && index<=endingCol;index++) {
             ans.push_back(matrix[startingRow][index]);
             count++;
         }
         startingRow++;
         
-        //print ending column
         for(int index = startingRow;count<total && index<=endingRow;index++) {
             ans.push_back(matrix[index][endingCol]);
             count++;
         }
         endingCol--;
         
-        //print ending row
         for(int index = endingCol;count<total && index >= startingCol;index--) {
             ans.push_back(matrix[endingRow][index]);
             count++;
         }
         endingRow--;
 
-        //print starting colunm
         for(int index= endingRow;count<total && index>=startingRow;index--) {
             ans.push_back(matrix[index][startingCol]);
             count++;
